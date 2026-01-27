f04c6cfa392063c7cc713ff2c1f30465620f398b
jrdiong <jiaraudiong@gmail.com>
2026-01-27 15:05:04 +0800

    setup

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Community Blood Donation Coordination App (CBDC App)</title>
+</head>
+<body>
+    
+</body>
+</html>
