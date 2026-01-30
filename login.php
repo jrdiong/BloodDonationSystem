bc101de02be7994da6beb99680a6526b93a81b9a
Junwee0314 <ness.junwee@gmail.com>
2026-01-30 19:42:52 +0800

    Update login.php

--- login.php
+++ login.php
@@ -45,7 +45,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST['login'])) {
             $_SESSION['name'] = $row['name'];
             $_SESSION['role'] = $row['role'];
 
-            header("Location: dashboard.php"); 
+            header("Location: test.html"); 
             exit();
         } else {
             echo "<script>
