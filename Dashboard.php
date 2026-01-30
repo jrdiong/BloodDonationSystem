3593353abf31940eb929eedb06b3c655227439c2
jrdiong <jiaraudiong@gmail.com>
2026-01-31 01:44:53 +0800

    navbar & dashboard

--- Dashboard.php
+++ Dashboard.php
@@ -1,11 +1,22 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Dashboard</title>
-</head>
-<body>
-    
-</body>
-</html>
+<?php
+session_start();
+
+if (!isset($_SESSION['role'])) {
+    header("Location: login.php");
+    exit;
+}
+
+$role = $_SESSION['role'];
+?>
+
+<div class="d-flex">
+
+    <!-- Load role navbar/sidebar -->
+    <?php include "navbar/navbar_$role.php"; ?>
+
+    <!-- Dashboard Content -->
+    <div class="flex-grow-1 p-4">
+        <?php include "dashboard/$role.php"; ?>
+    </div>
+
+</div>
