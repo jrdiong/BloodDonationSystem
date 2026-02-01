0332a924e8508c9c0a49cf4bbb7f773585b8334b
Junwee0314 <ness.junwee@gmail.com>
2026-02-01 12:27:39 +0800

    Update login.php

--- login.php
+++ login.php
@@ -45,7 +45,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST['login'])) {
             $_SESSION['name'] = $row['name'];
             $_SESSION['role'] = $row['role'];
 
-            header("Location: request_manage.html"); 
+            header("Location: event.html"); 
             exit();
         } else {
             echo "<script>
